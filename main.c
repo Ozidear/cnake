44f6209193e59b2f5b1fe657e834db8ea45c1225
Ozidear <omerru06@gmail.com>
2026-09-15 19:46:45 +0300

    rendered snake

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 
-#define GREEN "\033[32m"
+#define GREEN "\033[42m"
 #define RESET "\033[0m"
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+void move_cursor(int x, int y) {
+    printf("\033[%d;%dH", y, x);
+}
 
 void render_line(char symbol, int length) {
     char string[length + 1];
@@ -15,6 +26,7 @@ void render_line(char symbol, int length) {
 
 void render_board(int height) {
     int width = height * 2;
+    move_cursor(1, 1);
 
     printf(" ");
     render_line('-', width);
@@ -30,8 +42,20 @@ void render_board(int height) {
 }
 
 
+void render_snake(int position) {
+    move_cursor((position * 2), position + 1);
+    printf(GREEN "  " RESET);
+}
 
 
 int main() {
-    render_board(10);
+    int i = 1;
+    while (true) {
+        render_board(10);
+        render_snake(i);
+
+        i++;
+        fflush(stdout);
+        sleep(1);
+    }
 }
