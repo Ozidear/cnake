b96956b5be34e346fb38f7210ed40d6aa8c31146
Ozidear <omerru06@gmail.com>
2026-09-15 18:20:49 +0300

    drew a board

--- main.c
+++ main.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
+void draw_line(char symbol, int length) {
+    char string[length + 1];
+    for (int i = 0; i < length; i++) {
+        string[i] = symbol;
+    }
+    string[length] = '\0';
+    
+    printf("%s", string);
+}
+
+void draw_board(int height) {
+    int width = height * 2;
+
+    printf(" ");
+    draw_line('-', width);
+    printf("\n");
+    for (int i = 0; i < height; i++) {
+        printf("|");
+        draw_line(' ', width);
+        printf("|\n");
+    }
+    printf(" ");
+    draw_line('-', width);
+    printf("\n");
+}
+
+
 int main() {
-    printf("Hello world!\n");
+    draw_board(10);
 }
